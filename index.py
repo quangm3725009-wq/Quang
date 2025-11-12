62e9fb23aa7e688757f11e845c62fa0f48c7189d
quangm3725009-wq <vodangquang1982@gmail.com>
2025-11-12 20:04:24 +0700

    huong dan day code len

---
+++ index.py
@@ -0,0 +1,26 @@
+
+# PS F:\git> echo "# Quang" >> README.md 
+# tạo file README.md
+
+# PS F:\git> git init
+# tạo git trên github
+
+# Reinitialized existing Git repository in F:/git/.git/
+
+# PS F:\git> git add README.md
+# thêm file README.md lên github
+
+# PS F:\git> git commit -m "huong dan git" có thể thay đổi commit
+# commit cái file anh muốn
+
+
+
+# PS F:\git> git branch -M main
+# đưa file anh up lên lên nhánh chính của git
+
+# PS F:\git> git remote add origin https://github.com/quangm3725009-wq/Quang.git đường dẫn có thể thay đổi được
+# xác định đường dẫn để anh đưa file lên
+
+
+# PS F:\git> git push -u origin main
+# đưa file lên trên git
